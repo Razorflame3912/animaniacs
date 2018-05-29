a4d5b168e99b88d9028f9261662e66be48ee817e
Razorflame3912 <melsharawy@stuy.edu>
2018-05-29 01:41:15 -0400

    not working yet

--- my_main.c
+++ my_main.c
@@ -70,8 +70,49 @@ void first_pass() {
   //in order to use name and num_frames throughout
   //they must be extern variables
   extern int num_frames;
+  num_frames = 100;
   extern char name[128];
+  num_frames = 1;
+  memset(name,0,256);
+  int vary_found;
+  int frames_found;
+  int basename_found;
+  vary_found = 0;
+  frames_found = 1;
+  basename_found = 0;
+  int i;
+  for(i=0;i<lastop;i++){
+    printf("%d\n",op[i].opcode);
+    switch (op[i].opcode){
+    case FRAMES:
+      printf("%f",op[i].op.frames.num_frames);
+      num_frames = op[i].op.frames.num_frames;
+      frames_found = 1;
+      break;
+    case BASENAME:
+      strcpy(name,op[i].op.basename.p->name);
+      basename_found = 1;
+      break;
+    case VARY:
+      vary_found = 1;
+      break;
+    }
+    printf("Frames? %d\n", frames_found);
+    printf("Basename? %d\n", basename_found);
+    printf("Vary? %d\n\n", vary_found);
+  }
+  if(vary_found && !frames_found){
+    printf("Found a vary without frames. Exiting...\n");
+    exit(-1);
+  }
+  
+  if(frames_found && !basename_found){
+    printf("Found frames with no basename. Using 'def'...");
+    strcpy(name,"def");
+  }
 
+  printf("%s\n", name);
+  
 }
 
 /*======== struct vary_node ** second_pass() ==========
@@ -79,7 +120,7 @@ void first_pass() {
   Returns: An array of vary_node linked lists
 
   In order to set the knobs for animation, we need to keep
-  a seaprate value for each knob for each frame. We can do
+  a seperate value for each knob for each frame. We can do
   this by using an array of linked lists. Each array index
   will correspond to a frame (eg. knobs[0] would be the first
   frame, knobs[2] would be the 3rd frame and so on).
@@ -91,68 +132,68 @@ void first_pass() {
   Go through the opcode array, and when you find vary, go
   from knobs[0] to knobs[frames-1] and add (or modify) the
   vary_node corresponding to the given knob with the
-  appropirate value.
+  appropriate value.
   ====================*/
 struct vary_node ** second_pass() {
-  return NULL;
+  num_frames = 100;
+  struct vary_node ** knobs = (struct vary_node **)calloc(num_frames, sizeof(struct vary_node *));
+  int i;
+   for(i=0;i<lastop;i++){
+     double start_frame;
+     double end_frame;
+     double start_val;
+     double end_val;
+     double step;
+     int f;
+     int found = 0;
+     struct vary_node * addition = NULL;
+     printf("%d\n",op[i].opcode);
+     switch (op[i].opcode){
+     case VARY:
+       start_frame = op[i].op.vary.start_frame;
+       end_frame = op[i].op.vary.end_frame;
+       start_val = op[i].op.vary.start_val;
+       end_val = op[i].op.vary.end_val;
+
+       printf("%f\n", start_frame);
+       printf("%f\n", end_frame);
+       printf("%f\n", start_val);
+       printf("%d\n", num_frames);
+       printf("%f\n\n", end_val);
+       if(start_frame < 0 || end_frame < 0 || end_frame < start_frame || end_frame > num_frames){
+	printf("Arguments for vary are invalid.\n");
+	exit(-1);
+      }
+      step = (end_val - start_val)/(end_frame - start_frame);
+        for(f = 0; f < num_frames; f++){
+	addition = knobs[f];
+	while(addition){
+	  if (!strcmp( addition->name, op[i].op.vary.p->name)) {
+            found = 1;
+            break;
+	  }
+	}
+	if(!found){
+	  addition = (struct vary_node *)calloc(1,sizeof(struct vary_node));
+	  strcpy(addition->name,op[i].op.vary.p->name);
+	  addition->value = 0;
+	  addition->next = knobs[f];
+	  knobs[f] = addition;
+	}
+	if(start_frame <= f && f <= end_frame){
+	  addition->value = start_val + (step*(f-start_frame));
+	}
+      }
+      break;
+     }
+   }
+   return knobs;
 }
-
-/*======== void print_knobs() ==========
-Inputs:
-Returns:
-
-Goes through symtab and display all the knobs and their
-currnt values
-====================*/
-void print_knobs() {
+ 
+void draw_frames(struct vary_node ** knobs){
   int i;
 
-  printf( "ID\tNAME\t\tTYPE\t\tVALUE\n" );
-  for ( i=0; i < lastsym; i++ ) {
-
-    if ( symtab[i].type == SYM_VALUE ) {
-      printf( "%d\t%s\t\t", i, symtab[i].name );
-
-      printf( "SYM_VALUE\t");
-      printf( "%6.2f\n", symtab[i].s.value);
-    }
-  }
-}
-
-/*======== void my_main() ==========
-  Inputs:
-  Returns:
-
-  This is the main engine of the interpreter, it should
-  handle most of the commadns in mdl.
-
-  If frames is not present in the source (and therefore
-  num_frames is 1, then process_knobs should be called.
-
-  If frames is present, the enitre op array must be
-  applied frames time. At the end of each frame iteration
-  save the current screen to a file named the
-  provided basename plus a numeric string such that the
-  files will be listed in order, then clear the screen and
-  reset any other data structures that need it.
-
-  Important note: you cannot just name your files in
-  regular sequence, like pic0, pic1, pic2, pic3... if that
-  is done, then pic1, pic10, pic11... will come before pic2
-  and so on. In order to keep things clear, add leading 0s
-  to the numeric portion of the name. If you use sprintf,
-  you can use "%0xd" for this purpose. It will add at most
-  x 0s in front of a number, if needed, so if used correctly,
-  and x = 4, you would get numbers like 0001, 0002, 0011,
-  0487
-  ====================*/
-void my_main() {
-
-  int i;
-  struct matrix *tmp;
-  struct stack *systems;
-  screen t;
-  zbuffer zb;
+  struct vary_node *  x;
   color g;
   g.red = 0;
   g.green = 0;
@@ -160,6 +201,7 @@ void my_main() {
   double step_3d = 20;
   double theta;
   double knob_value, xval, yval, zval;
+  knob_value = 1;
 
   //Lighting values here for easy access
   color ambient;
@@ -197,13 +239,21 @@ void my_main() {
   sreflect[GREEN] = 0.5;
   sreflect[BLUE] = 0.5;
 
-  systems = new_stack();
-  tmp = new_matrix(4, 1000);
-  clear_screen( t );
-  clear_zbuffer(zb);
+  //clear_screen( t );
+  //clear_zbuffer(zb);
+
+
 
   for (i=0;i<lastop;i++) {
     //printf("%d: ",i);
+    struct matrix *tmp;
+    struct stack *systems;
+    screen t;
+    zbuffer zb;
+    clear_screen(t);
+    clear_zbuffer(zb);
+    systems = new_stack();
+    tmp = new_matrix(4, 1000);
     switch (op[i].opcode)
       {
       case SPHERE:
@@ -308,11 +358,15 @@ void my_main() {
         zval = op[i].op.move.d[2];
         printf("Move: %6.2f %6.2f %6.2f",
                xval, yval, zval);
+	x = knobs[i];
         if (op[i].op.move.p != NULL)
           {
-            printf("\tknob: %s",op[i].op.move.p->name);
+	    while(strcmp(x->name,op[i].op.scale.p->name)){
+	      x = x->next;
+	    }
+	    knob_value = x->value;
           }
-        tmp = make_translate( xval, yval, zval );
+        tmp = make_translate( xval * knob_value, yval * knob_value , zval * knob_value );
         matrix_mult(peek(systems), tmp);
         copy_matrix(tmp, peek(systems));
         tmp->lastcol = 0;
@@ -325,9 +379,13 @@ void my_main() {
                xval, yval, zval);
         if (op[i].op.scale.p != NULL)
           {
-            printf("\tknob: %s",op[i].op.scale.p->name);
+	    x = knobs[i];
+            while(strcmp(x->name,op[i].op.scale.p->name)){
+	      x = x->next;
+	    }
+	    knob_value = x->value;
           }
-        tmp = make_scale( xval, yval, zval );
+        tmp = make_scale( xval * knob_value, yval *  knob_value, zval * knob_value);
         matrix_mult(peek(systems), tmp);
         copy_matrix(tmp, peek(systems));
         tmp->lastcol = 0;
@@ -340,14 +398,18 @@ void my_main() {
         if (op[i].op.rotate.p != NULL)
           {
             printf("\tknob: %s",op[i].op.rotate.p->name);
-          }
+            while(strcmp(x->name,op[i].op.scale.p->name)){
+	      x = x->next;
+	    } 
+	    knob_value = x->value;
+         }
         theta*= (M_PI / 180);
         if (op[i].op.rotate.axis == 0 )
-          tmp = make_rotX( theta );
+          tmp = make_rotX( theta * knob_value);
         else if (op[i].op.rotate.axis == 1 )
-          tmp = make_rotY( theta );
+          tmp = make_rotY( theta * knob_value);
         else
-          tmp = make_rotZ( theta );
+          tmp = make_rotZ( theta * knob_value);
 
         matrix_mult(peek(systems), tmp);
         copy_matrix(tmp, peek(systems));
@@ -363,13 +425,77 @@ void my_main() {
         break;
       case SAVE:
         //printf("Save: %s",op[i].op.save.p->name);
-        save_extension(t, op[i].op.save.p->name);
+	save_extension(t, op[i].op.save.p->name);
         break;
       case DISPLAY:
         //printf("Display");
         display(t);
         break;
       } //end opcode switch
+    if(num_frames > 1){
+      char savename[256];
+      sprintf(savename, "%s/%s%05d.png", name, name, i);
+      printf("%s\n",savename);
+      save_extension(t, savename);
+    }
     printf("\n");
   }//end operation loop
 }
+
+/*======== void print_knobs() ==========
+Inputs:
+Returns:
+
+Goes through symtab and display all the knobs and their
+currnt values
+====================*/
+void print_knobs() {
+  int i;
+
+  printf( "ID\tNAME\t\tTYPE\t\tVALUE\n" );
+  for ( i=0; i < lastsym; i++ ) {
+
+    if ( symtab[i].type == SYM_VALUE ) {
+      printf( "%d\t%s\t\t", i, symtab[i].name );
+
+      printf( "SYM_VALUE\t");
+      printf( "%6.2f\n", symtab[i].s.value);
+    }
+  }
+}
+
+/*======== void my_main() ==========
+  Inputs:
+  Returns:
+
+  This is the main engine of the interpreter, it should
+  handle most of the commadns in mdl.
+
+  If frames is not present in the source (and therefore
+  num_frames is 1, then process_knobs should be called.
+
+  If frames is present, the enitre op array must be
+  applied frames time. At the end of each frame iteration
+  save the current screen to a file named the
+  provided basename plus a numeric string such that the
+  files will be listed in order, then clear the screen and
+  reset any other data structures that need it.
+
+  Important note: you cannot just name your files in
+  regular sequence, like pic0, pic1, pic2, pic3... if that
+  is done, then pic1, pic10, pic11... will come before pic2
+  and so on. In order to keep things clear, add leading 0s
+  to the numeric portion of the name. If you use sprintf,
+  you can use "%0xd" for this purpose. It will add at most
+  x 0s in front of a number, if needed, so if used correctly,
+  and x = 4, you would get numbers like 0001, 0002, 0011,
+  0487
+  ====================*/
+void my_main() {
+  first_pass();
+  printf("Made it past first pass!\n");
+  struct vary_node ** knobs = second_pass();
+  printf("Made it past second pass!\n");
+  draw_frames(knobs);
+}
+
